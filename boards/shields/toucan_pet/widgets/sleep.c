08efa61cd02016a507883db28ab2e159c160b0de
alee0729 <albert.c.lee@gmail.com>
2026-03-15 13:31:50 -0400

    Update sleep.c

--- boards/shields/toucan_pet/widgets/sleep.c
+++ boards/shields/toucan_pet/widgets/sleep.c
@@ -156,3 +156,17 @@ bool is_sleep_screen_active(void) {
 void set_sleep_screen_active(bool active) {
     show_sleep_screen = active;
 }
+
+void draw_sleep_screen(lv_obj_t *canvas) {
+    // Draw the sleep icon centered horizontally, near top of screen
+    lv_draw_img_dsc_t img_dsc;
+    lv_draw_img_dsc_init(&img_dsc);
+    // Center the 96px icon on the 144px wide screen: (144-96)/2 = 24
+    // Draw it a bit down from the top: y=32
+    lv_canvas_draw_img(canvas, 8, 8, &sleep_icon, &img_dsc);
+
+    // Show "SLEEP" in small text below the icon
+    lv_draw_label_dsc_t label_dsc;
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &quinquefive_8, LV_TEXT_ALIGN_LEFT);
+    lv_canvas_draw_text(canvas, 12, 140, SCREEN_WIDTH-8, &label_dsc, "SLEEP");
+}
